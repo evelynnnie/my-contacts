9e967fc29f75d8e3ad8217b819a4f3ba7fbcd801
evelynnnie <deparaujo4907@gmail.com>
2026-07-17 21:39:06 -0300

    feat: criar classes de modelo Contato e ContatoComercial

--- src/model/ContatoComercial.java
+++ src/model/ContatoComercial.java
@@ -1,5 +1,24 @@
 package model;
 
-public class ContatoComercial {
+public class ContatoComercial extends Contato {
+    private String empresa;
     
+    public ContatoComercial(String nome, String telefone, String email, String empresa){
+        super(nome, telefone, email);
+        this.empresa = empresa;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + "| Empresa: " + empresa;
+        
+    }
 }
